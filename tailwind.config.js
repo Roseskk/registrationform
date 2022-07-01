a913a708aa5aa5a920e9386dfc9424ed13d145ca
Roseskk <oleg419583@yandex.ru>
2022-07-01 16:40:04 +0300

    initial coomit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        pageW: '1600px'
+      },
+      width: {
+        a4: '210mm'
+      },
+      height:{
+        a4: '297mm'
+      },
+      fontSize:{
+        xxs: '0.4rem',
+        x2s: '0.4rem'
+      }
+    },
+  },
+  plugins: [],
+}
